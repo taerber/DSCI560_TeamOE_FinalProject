13de7dc816a2d2cfeeb08bc8c3ebe8789e2973be
Lucas Huang <lmhuang@usc.edu>
2023-05-01 23:14:11 -0700

    Files updated

--- README.md
+++ README.md
@@ -1,41 +1,59 @@
-How to deploy our website
+## Deployment
 
-Step 1: Save somewhere onto your computer
+In the project directory, run the following 2 commands:
 
-Step 2: open a terminal window
+#### `pip install -r requirements.txt`
 
-Step 3: cd to the location of the FinalProject folder
+#### `python backend.py`
 
-Step 4: run Python backend.py
+This installs all dependences and runs the app. Open the URL given in your terminal to view it in your browser.\
 
-Step 5: copy and paste the url given into a chrome browser(please use chrome as there may be cache problems with other browsers)
+## Task Specifics
+### Routing Drivers
+This script is a route planner that finds a suitable stop location between a start and end address based on the maximum range of an EV and an average speed limit close to 55 mph. It reads a CSV file containing potential charger locations and calculates the route using Google Maps API.
 
-Step 6: Click around on the website! When entering routes please make sure the destination and starting location are far apart, if the locations are too close and the mileage entered is enough to get from point A to B then the routing doesn't work because of the buffer we have added in place(15-50 miles), if this happens just click back to the previous page and retry a different remaining mileage or locations
+To run the alogrithm and obtain routing results maually:
+
+#### `python script.py <start_address> <end_address> <max_distance_miles> <csv_file_path>`
+> `<start_address>`: The starting address (e.g., "123 Main St, Anytown, USA").  
+`<end_address>`: The ending address (e.g., "789 Elm St, Othertown, USA").  
+`<max_distance_miles>`: The maximum distance (in miles) the vehicle can travel before needing to stop.  
+`<csv_file_path>`: The path to the CSV file containing potential stop locations.
+
+When entering routes please make sure the destination and starting location are far apart, if the locations are too close and the mileage entered is enough to get from point A to B then the routing doesn't work because of the buffer we have added for a user's range of 15-50 miles. If this happens, click back to the previous page and retry a different remaining mileage or locations.
 
 Some Example Routes:
-
-Destination: 650 Gateway Center Dr, San Diego, CA 92102
-Current Location: 31790 Oak Ranch CT Westlake Village CA 91361
-Remaining Mileage: 100
+> Destination: 650 Gateway Center Dr, San Diego, CA 92102  
+Current Location: 31790 Oak Ranch CT Westlake Village CA 91361  
+Remaining Mileage: 100  
 Total Mileage: 200
 
-Destination: PIER 39 The Embarcadero &, Beach St, San Francisco, CA 94133
-Current Location: 31790 Oak Ranch CT Westlake Village CA 91361
-Remaining Mileage: 200
+> Destination: PIER 39 The Embarcadero &, Beach St, San Francisco, CA 94133  
+Current Location: 31790 Oak Ranch CT Westlake Village CA 91361  
+Remaining Mileage: 200  
 Total Mileage: 400
 
-Destination: 886 Cannery Row, Monterey, CA 93940
-Current Location: 1 UCEN Rd, Santa Barbara, CA 93106
-Remaining Mileage: 100
+> Destination: 886 Cannery Row, Monterey, CA 93940  
+Current Location: 1 UCEN Rd, Santa Barbara, CA 93106  
+Remaining Mileage: 100  
 Total Mileage: 200
 
-- for our route page when entering locations the map may not refresh, this has to due to how the browser caches, please use Chrome to avoid these problems, if the issue persists then please just clear your browsers history and you can open the 'route_map.html' in the templates folder in a browser and will see the map file does create the correct map but may not refresh in the route html.
+### Prediting Charger Locations
+This script is a location predictor that finds a set of optimal locations for electric vehicle chargers. It utilizes an optimization problem model that evaluates sets of charger locations to find which maximizes our evaluation metric: the total population of electric vehicle users within 10 miles of at least one of the stations in the set. To get a pool of potential charger locations to optimize on, our model creates a grid centered on a city (user input, as described below) with 5 mile separation beetween points within the grid. Each point represents a potential charger location. The algorithm also utilizes density-based clustering algorithm HDBSCAN to cluster existing charger locations to facilitate a visualization of current charger coverage; this was an important development of our model such that the efficiency of our model could be visualied in comparing outputted results to the current distribution of chargers.
 
+To run the alogrithm and obtain routing results maually:
 
-Problems that could arise
+#### `python loation_prediction_mclp.py <num_chargers> <city>`
+> `<num_chargers>`: The desired number of optimal charger locations to find.  
+`<city>`: The desired city to find optimal electric vehicle charger locations within a 75 mile radius.
 
-- for backend to run all the import packages need to be installed
+The model outputs 3 .html maps: 
+- mclp-new-coverage-map.html draws optimal charger locations with 10 mile size circles. This output is best when desiring visual results, for visualizing the coverage of the found optimal configuration of charging stations.
+- mclp-new-location-map.html draws optimal charger locations with simple markers. This output is best when desiring a clean view, for detailed examination of the latitude/longitude optimal locations that the model outputs. 
+- mclp.html draws both a circle and marker as detailed above for each optimal charger location.
 
-- due to computer enviroment differences(since dsci 553 requires us to use an older version of spark and my computer uses anaconda also I have too many versions of spark so the file can't correctly find the same versions) the kmeans_pred.py file cannot be run in the backend.py, so all the html maps that python file reproduces have been pre-created and entered manually into the backend.py, but they are produced by the kmeans_pred.py file and are accurate 
+## Known Issues
 
+- For our route page, when entering locations the map may not refresh. You can open the 'route_map.html' in the templates folder in a browser and see that the map file creates the correct map. This visualization issue is due to how browsers handle caching; due to this, we recommend using the web browser Google Chrome to avoid such problems. If the issue persists, clear your browsers cache. 
 
+- Due to computer enviroment differences, we encountered difficulties importing and running our predictive location algorithm file in the overall project's backend.py. Due to this, we stored our algorithm's .html map output of predicted locations locally. To verify results or utilize the algorithm with different parameters, please follow the steps outline above in "Task Specifics". 
---
+++ location_prediction_mclp.py
@@ -0,0 +1,186 @@
+import pandas as pd
+import numpy as np
+from pyspark.conf import SparkConf
+from pyspark.context import SparkContext
+from operator import add
+from uszipcode import SearchEngine
+import gmplot
+from shapely.geometry import MultiPoint, Polygon
+import gurobipy as gp
+from gurobipy import GRB
+from geopy.distance import great_circle
+from collections import defaultdict
+import haversine as hs
+from scipy.spatial import ConvexHull
+import hdbscan
+from global_land_mask import globe
+
+apikey = "AIzaSyDv0JWXkfts9An4XKSkHXgrU0SxEyOu2CQ"
+
+def maximal_coverage(city, num_predictions=50, state="CA"):
+    conf = SparkConf()
+    conf.setMaster("local").setAppName("My app")
+    sc = SparkContext(conf=conf)
+
+    # Columns : ['Data Year', 'Fuel Type', 'ZIP', 'Number of Vehicles']
+    data = sc.textFile("New_ZEV_Sales_Last_updated_01-18-2023.csv").filter(lambda row : row.split(',')[2] != 'ZIP')
+    # (zip, number of vehicles)
+    # # First filter for sales in last 5 years
+    data_rdd = data.filter(lambda row : int(row.split(',')[2]) >= 2018).map(lambda row : (row.split(',')[2], int(row.split(',')[3])))
+    data_existing_chargers = pd.read_csv('alt_fuel_stations_Feb_2_2023.csv', usecols=['Latitude', 'Longitude'])
+    existing_chargers = data_existing_chargers[['Latitude', 'Longitude']].values.tolist()
+
+    # not real zipcodes?
+    fake_zips = ["94003", "94029", "92670", "92680", "99999"]
+    zipcode_sales = data_rdd.reduceByKey(add).filter(lambda row : row[0] not in fake_zips) \
+        .sortBy(lambda row : -row[1]).collectAsMap()
+    # https://uszipcode.readthedocs.io/01-Usage-Example/index.html
+    # https://uszipcode.readthedocs.io/02-Advanced-Tutorial/index.html
+    search = SearchEngine()
+    source_result = search.by_city_and_state(city=city, state=state)[0]
+    source_latlng = tuple([source_result.lat, source_result.lng])
+
+    def latlng_from_zip(zipcode):
+        result = search.by_zipcode(zipcode)
+        return tuple([float(result.lat), float(result.lng)])
+    latlng_sales = {}
+    for zipcode in zipcode_sales:
+        lat_lng = latlng_from_zip(zipcode)
+        # https://pypi.org/project/haversine/
+        if hs.haversine(source_latlng, lat_lng, unit='mi') < 75:
+            latlng_sales[lat_lng] = zipcode_sales[zipcode]  
+    demand_points = list(latlng_sales.keys())
+
+    def get_centermost_point(cluster):
+        centroid = (MultiPoint(cluster).centroid.x, MultiPoint(cluster).centroid.y)
+        centermost_point = min(cluster, key=lambda point: great_circle(point, centroid).m)
+        return tuple(centermost_point)
+    charging_stations = []
+    for station in existing_chargers:   
+        if hs.haversine(source_latlng, station, unit='mi') < 75:
+            charging_stations.append(tuple(station))
+
+    # https://hdbscan.readthedocs.io/en/latest/api.html
+    # https://hdbscan.readthedocs.io/en/latest/parameter_selection.html
+    # Cluster existing charger locations to get clusters of "coverage"
+    # # Haversine distance uses radians
+    # # Merge clusters within a distance of 1 miles
+    miles_per_radian = 3959.0
+    hdbs = hdbscan.HDBSCAN(min_cluster_size=5, metric='haversine', min_samples=1, cluster_selection_epsilon=(1.0 / miles_per_radian)).fit(np.radians(charging_stations))
+    # -1 cluster represents data points of noise
+    clusters_chargers = sc.parallelize(hdbs.labels_).zipWithIndex().map(lambda row : (row[0], charging_stations[row[1]])) \
+        .groupByKey().map(lambda row : (row[0], list(row[1])))            
+    # find the point in each cluster that is closest to its centroid
+    charger_cluster_center_points = clusters_chargers.filter(lambda row : row[0] != -1).map(lambda row : get_centermost_point(row[1])).collect()
+
+    # https://docs.scipy.org/doc/scipy/reference/generated/scipy.spatial.ConvexHull.html
+    # https://shapely.readthedocs.io/en/stable/manual.html#shapely.geometry.shape
+    # Get potental locations, points within the grid formed by demand points
+    # # Our grid has points spaced 0.07 degrees (about 5 miles) apart
+    convex_hull = ConvexHull(demand_points)
+    # # (min x, min y, max x, max y) where x = lat, y = lng
+    demand_points_shape = list(Polygon([demand_points[idx] for idx in convex_hull.vertices]).bounds)
+    lat_min = demand_points_shape[0]
+    lat_max = demand_points_shape[2]
+    lon_min = demand_points_shape[1]
+    lon_max = demand_points_shape[3]
+    lat = lat_min
+    lon = lon_min
+    potential_locations = []
+    while True:
+        if lon > lon_max:
+            break
+        lat = lat_min
+        while True:
+            if lat > lat_max:
+                break
+            # Make sure our potential location is on land (not in the ocean)
+            if globe.is_land(lat, lon):
+                potential_locations.append(tuple([lat, lon]))
+            lat += 0.07
+        lon += 0.07
+
+    # https://www.gurobi.com/documentation/9.5/refman/py_model2.html    
+    model = gp.Model("MCLP")
+    # Add all potential locations as x variables
+    # # 1 if location is used in optimal solution, 0 if not
+    x = {}
+    for charger_idx in range(len(potential_locations)):
+        x[charger_idx] = model.addVar(vtype=GRB.BINARY, name="x_" + str(charger_idx))
+    # Add all demand nodes as y variables
+    # # 1 when covered, 0 when not
+    y = {}
+    for location_idx in range(len(demand_points)):
+        y[location_idx] = model.addVar(vtype=GRB.BINARY, name="y_" + str(location_idx))
+    
+    # Map demand points to all potential charging locations within 10 miles, which is our criteria for coverage
+    valid_locations = defaultdict(set)
+    for demand_point in range(len(demand_points)):
+        for location in range(len(potential_locations)):  
+            if hs.haversine(demand_points[demand_point], potential_locations[location], unit='mi') < 10:
+                valid_locations[demand_point].add(location)
+
+    # Create two constraints for our MCLP 
+    # # len(number of valid locations) >= y for every demand point
+    for demand_point in range(len(demand_points)):
+        # # if 1+ valid locations, our condition is true
+        if valid_locations[demand_point]:
+            model.addConstr(sum([x[potential_location] for potential_location in valid_locations[demand_point]]) >= y[demand_point])
+        # # if 0 valid locations, y should be 0 to represent not covered
+        else:
+            model.addConstr(y[demand_point] == 0)
+    # # Number of final predicted locations
+    model.addConstr(sum([x[location] for location in range(len(potential_locations))]) == num_predictions)
+    
+    # Maximize population covered
+    # # Maximize total sum over all demand locations: (sales in location) * (1 if covered, 0 if not covered)
+    model.setObjective(sum([latlng_sales[demand_points[point]] * y[point] for point in range(len(demand_points))]), GRB.MAXIMIZE)
+    model.update()
+    model.optimize()
+
+    # https://www.gurobi.com/documentation/10.0/quickstart_windows/cs_example_mip1_py.html
+    final_charger_locations = []
+    final_covered_locations = []
+    for v in model.getVars():
+        curr_variable = v.varName.split("_")
+        if curr_variable[0] == "x" and v.X == 1:
+            final_charger_locations.append(int(curr_variable[1]))
+        elif curr_variable[0] == "y" and v.X == 1:
+            final_covered_locations.append(int(curr_variable[1]))
+
+    print("{:,} charger points clustered to {:,} points, {:.3f}.".format(len(charging_stations), len(charger_cluster_center_points), len(charger_cluster_center_points) / len(charging_stations)))
+    print("Model considered {:,} potential locations for new chargers.".format(len(potential_locations)))    
+    print("{:,} optimal charger locations found.".format(len(final_charger_locations)))    
+    print("{:,} demand points covered at conclusion of model, {:.3f}.".format(len(final_covered_locations), len(final_covered_locations) / len(demand_points))) 
+    
+    final_predicted_locations = [potential_locations[location] for location in final_charger_locations]
+
+    miles_per_meter = 0.000621371
+    def scatter_new_coverage():
+        gmap = gmplot.GoogleMapPlotter(lat=source_latlng[0], lng=source_latlng[1], zoom=10, apikey=apikey)
+        gmap.scatter(*zip(*final_predicted_locations), color='red', marker=False, size=(10.0 / miles_per_meter))
+        gmap.scatter(*zip(*charger_cluster_center_points), color='green', marker=True)
+        gmap.draw('mclp-new-coverage-map.html')    
+    def scatter_new_marker():
+        gmap = gmplot.GoogleMapPlotter(lat=source_latlng[0], lng=source_latlng[1], zoom=10, apikey=apikey)
+        gmap.scatter(*zip(*final_predicted_locations), color='red', marker=True)
+        gmap.scatter(*zip(*charger_cluster_center_points), color='green', marker=True)
+        gmap.draw('mclp-new-location-map.html')    
+    def scatter_new_both():
+        gmap = gmplot.GoogleMapPlotter(lat=source_latlng[0], lng=source_latlng[1], zoom=10, apikey=apikey)
+        gmap.scatter(*zip(*final_predicted_locations), color='red', marker=True)
+        gmap.scatter(*zip(*final_predicted_locations), color='red', marker=False, size=(10.0 / miles_per_meter))
+        gmap.scatter(*zip(*charger_cluster_center_points), color='green', marker=True)
+        gmap.draw('mclp.html')    
+    scatter_new_coverage()
+    scatter_new_marker()
+    scatter_new_both()
+
+def main():
+    import sys
+    input_numpreds = int(sys.argv[1])
+    input_city = ' '.join(sys.argv[2:])
+    maximal_coverage(city=input_city, num_predictions=input_numpreds)
+
+if __name__ == "__main__":
+    main()
